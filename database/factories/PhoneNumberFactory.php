11b513ba4f26848582fa6386c0a5d08e93bf28e5
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-20 16:07:41 +0200

    update factory

--- database/factories/PhoneNumberFactory.php
+++ database/factories/PhoneNumberFactory.php
@@ -2,8 +2,8 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Contact;
-use App\PhoneNumber;
+use App\Models\Contact;
+use App\Models\PhoneNumber;
 use Faker\Generator as Faker;
 
 $factory->define(PhoneNumber::class, function (Faker $faker) {
