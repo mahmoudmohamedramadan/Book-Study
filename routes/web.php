3691e97ce5fdae28ae21f3f75122032955b02134
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-22 11:51:59 +0200

    update web route file

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,37 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\UserCommentsController;
 
 Auth::routes();
 
-Route::view('/', 'welcome')->name('welcome');
-
 Route::view('home', 'home');
+
+Route::view('/', 'welcome')->name('welcome');
 
 Route::group(['prefix' => 'user/login'], function () {
     Route::get('/', 'LoginController@loginForm');
     Route::post('/', 'LoginController@login')->name('user.login');
+});
+
+Route::group(['prefix' => 'posts/{post}/comments'], function () {
+    //route model binding
+    Route::post('/', [CommentsController::class, 'store'])->name('comments.store');
+    Route::get('{comment}/edit', [CommentsController::class, 'edit'])->name('comments.edit');
+    Route::patch('{comment}', [CommentsController::class, 'update'])->name('comments.update');
+    Route::delete('{comment}', [CommentsController::class, 'destroy'])->name('comments.destroy');
+});
+
+Route::resource('posts', 'PostController');
+
+Route::name('user.')->prefix('user')->group(function () {
+    Route::name('comments.')->prefix('{user}/comments')->group(function () {
+        Route::get('{comment}', [UserCommentsController::class, 'show'])->name('show');
+    });
 });
 
 Route::get('alert', [UserController::class, 'returnAlert']);
