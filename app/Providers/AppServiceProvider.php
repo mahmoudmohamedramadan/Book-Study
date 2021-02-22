5089f9ccf4e8201a60632910f571bd5a86653a1e
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-22 11:50:44 +0200

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Http\Composer\UserComposer;
-use Illuminate\Support\Facades\Blade;
-use Illuminate\Support\Facades\View;
+use App\Models\Contact;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,13 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //NAMESPACE OF VIEW IS: Illuminate\Support\Facades\View NOT View\View
         //View::share('users', User::get());
 
         // View::composer('home', function ($view) {
         //     $view->with('users', User::get());
         // });
 
-        View::composer('home', UserComposer::class);
+        // View::composer('home', UserComposer::class);
 
         // Blade::component('book-component', 'modal');
 
@@ -40,13 +39,32 @@ class AppServiceProvider extends ServiceProvider
             return "<?php if(auth()->guest()) { ?>";
         });*/
 
-        //instead of directive i can use if function to customize check
+        /*instead of directive i can use if function to customize check
         Blade::if('ifGuest', function() {
             return auth()->guest();
         });
 
         Blade::directive('endIfGuest', function() {
             return '<?php } ?>';
+        });*/
+
+        Contact::creating(function() {
+            dump('contact is creating now...');
         });
+
+        Contact::created(function() {
+            dump('contact is created successfully');
+        });
+
+        //AND IN GENERAL VIEW YOU CAN DO IT >> Modelname::eventName()
+        // Contact::saved(function() {});
+        // Contact::saving(function() {});
+        // Contact::updated(function() {});
+        // Contact::updating(function() {});
+        // Contact::deleted(function() {});
+        // Contact::deleting(function() {});
+        // Contact::restored(function() {});
+        // Contact::restoring(function() {});
+        // Contact::retrieved(function() {});
     }
 }
