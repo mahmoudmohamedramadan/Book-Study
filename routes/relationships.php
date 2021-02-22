77ff741fb28d26e52ebb074f11a02221fd7a6797
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-22 11:51:43 +0200

    add some functions in relationships route file

--- routes/relationships.php
+++ routes/relationships.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\User;
+use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
 
 Route::get('relationships', function () {
@@ -80,11 +80,20 @@ Route::get('relationships', function () {
     // $user = User::whereHas('phoneNumbers')->get();
 
     //HERE AFTER GETTING ALL USERS WHICH HAVE phoneNumbers WILL FIND BETWEEN THEM WHICH USER HAS A ID EQUALS 6
-    $user = User::whereHas('phoneNumbers')->get();
+    // $user = User::whereHas('phoneNumbers')->get();
 
     // $user = User::whereHas('phoneNumbers', function ($query) {
     //     return $query->where('contact_id', 5);
     // })->get();
 
-    return $user;
+    // $contacts = Contact::get();
+
+    //THIS TYPE CALLED LAZY EAGER LOADING
+    // return $contacts->load('phoneNumber');
+
+    // return $contacts->loadMissing('phoneNumber');
+    //VISIT THIS LINK FOR MORE INFO: https://laravel.com/docs/5.2/eloquent-relationships#eager-loading
+
+    //GET COUNT OF ITEMS IN EACH RELATIONSHIPS, THE NAME OF RELATIONSHIP SUFFIXED WITH COUNT IN SNAKE CASE
+    return Contact::withCount('phoneNumber')->get();
 });
