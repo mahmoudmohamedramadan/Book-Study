2969df9c2a73a4e849a2cc2e9b418107eec098f6
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-22 11:51:00 +0200

    minimize book route file

--- routes/book.php
+++ routes/book.php
@@ -1,24 +1,6 @@
 <?php
 
-use App\Http\Controllers\UserCommentsController;
-use App\Http\Controllers\CommentsController;
 use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix' => 'posts/{post}/comments'], function () {
-    //route model binding
-    Route::post('/', [CommentsController::class, 'store'])->name('comments.store');
-    Route::get('{comment}/edit', [CommentsController::class, 'edit'])->name('comments.edit');
-    Route::patch('{comment}', [CommentsController::class, 'update'])->name('comments.update');
-    Route::delete('{comment}', [CommentsController::class, 'destroy'])->name('comments.destroy');
-});
-
-Route::resource('posts', 'PostController');
-
-Route::name('user.')->prefix('user')->group(function () {
-    Route::name('comments.')->prefix('{user}/comments')->group(function () {
-        Route::get('{comment}', [UserCommentsController::class, 'show'])->name('show');
-    });
-});
 
 Route::apiResource('userApi', 'UserApiController');
 
@@ -51,6 +33,9 @@ Route::get('component', function () {
 });
 
 Route::get('collection', 'CollectionController@index');
+
+//BEFORE EMBARKING STORING CONTACT THE boot METHOD INSIDE AppServiceProvider WILL TRIGGERED AND THE eventName METHOD WILL CALLED
+Route::get('storeContact', 'ContactController@store');
 
 //laravel will resolve this function if your url not match any route
 Route::fallback(function () {
