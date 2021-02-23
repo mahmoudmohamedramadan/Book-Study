667cf5e38f4e289dccd4ab64f383924a5a96a6e3
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-23 12:50:16 +0200

    update collection of department

--- app/Http/Collections/DepartmentCollection.php
+++ app/Http/Collections/DepartmentCollection.php
@@ -10,7 +10,7 @@ class DepartmentCollection extends Collection
     public function reduceDepartmentCount()
     {
         // if you want to return all collection type... return $this
-        //using carry and item you show all item inthe collection, $carry used to merge last iteration with the next one visit to more info...https://riptutorial.com/laravel/example/12397/using-reduce--#:~:text=Laravel%20Using%20reduce()&text=The%20reduce%20method%20reduces%20the,result%20to%20the%20next%20iteration.
+        // using carry and item you show all item in the collection, $carry used to merge last iteration with the next one visit to more info...https://riptutorial.com/laravel/example/12397/using-reduce--#:~:text=Laravel%20Using%20reduce()&text=The%20reduce%20method%20reduces%20the,result%20to%20the%20next%20iteration.
         return $this->reduce(function ($carry, $item) {
             return  $item['name'] .' >> '.$carry;
         });
