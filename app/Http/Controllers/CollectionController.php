c185b6aaa16d8f0a2a60a9f05f123a4581f59d07
mahmoudmohamedramadan <mahmoudramadan496@gmail.com>
2021-02-23 12:49:38 +0200

    update collection controller

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -8,21 +8,25 @@ class CollectionController extends Controller
 {
     public function index()
     {
-        // $nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        // $collection = collect($nums);
+    //     $nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //     $collection = collect($nums);
 
+        // reject method will return the reverse of inner condition, this means this wil return odd numbers only
         // $odds = $collection->reject(function($num) {
         //     return $num % 2 === 0;
         // });
 
+        // filter method will return results depending on he condition, thios means this will return even numbers only
         // return $collection->filter(function ($num) {
         //     return $num % 2 === 0;
         // });
 
+        // map method used to get something performed in each iterator
         // return $collection->map(function ($num) {
         //     return $num * 10;
         // });
 
+        // here will reurn even numbers only then multiply each number in 10 and finally get sum of all numbers
         // return $collection
         //     ->filter(function ($num) {
         //         return $num % 2 === 0;
@@ -39,6 +43,7 @@ class CollectionController extends Controller
 
         $departments = Department::get();
 
+        //reduceDepartmentCount is method inside `DepartmentCollection` colletion class which i assign it to Department model
         return $departments->reduceDepartmentCount();
     }
 }
